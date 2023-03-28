62b6d673c5a1342ed9aee13f73ac01f34b76646a
Seyma SARIGİL <seymasargil@gmail.com>
2023-03-29 01:47:44 +0300

    updated json format

--- api/controllers/model_controller.py
+++ api/controllers/model_controller.py
@@ -10,13 +10,17 @@ model_router = APIRouter()
 @model_router.post("/prediction")
 async def get_label_score(items: Items, turkish_char: bool) -> dict:
     """
-    This endpoint takes a list of text inputs in the 'texts' field of the Items class and a boolean value
-    for the 'turkish_char' parameter. It preprocesses the text inputs, encodes them with the BERT tokenizer,
-    and feeds them to a pre-trained BERT model (dbmdz/bert-base-turkish-128k-uncased) to perform a text
-    classification. The predicted labels and whether each text is offensive or not are returned in a list of
-    dictionaries.
-    If the prediction label is 1, the text is considered offensive, and the 'is_offensive' key in the output
-    dictionary will be 0. Otherwise, the 'is_offensive' key will be 1.
+    This endpoint that defines an endpoint for a FastAPI router.
+    It takes in a list of text inputs (items) and a boolean value (turkish_char)
+    indicating whether to replace non-Turkish characters with their Turkish counterparts.
+    It then preprocesses the text inputs, encodes them with a BERT tokenizer,
+    and feeds them to a pre-trained
+    BERT model for Turkish language available at 'https://huggingface.co/dbmdz/bert-base-turkish-128k-uncased'
+    to perform a text classification.
+    The predicted labels and whether each text is offensive or not are returned in a list of dictionaries.
+    If the prediction label is "OTHER", the text is considered not offensive,
+    and the "is_offensive" key in the output dictionary will be 1. Otherwise, the "is_offensive" key will be 0.
+    The function returns a dictionary containing the predicted labels and scores.
     
     Parameters:
     -----------
@@ -42,16 +46,10 @@ async def get_label_score(items: Items, turkish_char: bool) -> dict:
     processed_text = response.json()["result"]
 
     pipeline = TextClassificationPipeline(model=wsgi.bert_model, tokenizer=wsgi.bert_tokenizer)
+
     results = pipeline(processed_text)
 
-    pred_list = [result["label"] for result in results]
-
-    response_body = {
-        "result": {
-            "model": [{label: 1 if label != "OTHER" else 0} for label in pred_list],
-            "texts": items.texts,
-        }
-    }
-
+    pred_list = [{"prediction": result["label"], "is_offensive": 0 if result["label"] == "OTHER" else 1} for result in
+                 results]
+    response_body = {"result": {"model": pred_list, "texts": items.texts}}
     return response_body
-
