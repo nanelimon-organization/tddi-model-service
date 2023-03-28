36f5b03e07b645475cfe5610e8ba4cd2c7f70fb1
TarikKaanKoc <tarikkaan1koc@gmail.com>
2023-03-28 13:18:38 +0300

    Upd response body

--- api/controllers/model_controller.py
+++ api/controllers/model_controller.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from transformers import TextClassificationPipeline
 import wsgi
 from api.models.requests import Items
+from collections import defaultdict
 
 model_router = APIRouter()
 
@@ -17,16 +18,19 @@ async def get_label_score(items: Items, turkish_char: bool) -> dict:
     dictionaries.
     If the prediction label is 1, the text is considered offensive, and the 'is_offensive' key in the output
     dictionary will be 0. Otherwise, the 'is_offensive' key will be 1.
+    
     Parameters:
     -----------
     items : Items
         An instance of the Items class that contains a list of text inputs.
     turkish_char : bool
         A boolean value indicating whether to replace non-Turkish characters with their Turkish counterparts.
+
     Returns:
     --------
     return : dictionary
-        A dictionary containing the predicted labels and indicators for whether each text is offensive or not.
+        A dictionary containing the predicted labels and scores.
+    
     NOTE:
         This function uses a pre-trained BERT model for Turkish language available at
         'https://huggingface.co/dbmdz/bert-base-turkish-128k-uncased'.
@@ -41,5 +45,14 @@ async def get_label_score(items: Items, turkish_char: bool) -> dict:
     pipeline = TextClassificationPipeline(model=wsgi.bert_model, tokenizer=wsgi.bert_tokenizer)
     results = pipeline(processed_text)
 
-    return {"success": True,
-            "payload": results}
+    pred_list = [result["label"] for result in results]
+
+    response_body = {
+        "result": {
+            "model": [{label: 1 if label != "OTHER" else 0} for label in pred_list],
+            "texts": items.texts,
+        }
+    }
+
+    return response_body
+
